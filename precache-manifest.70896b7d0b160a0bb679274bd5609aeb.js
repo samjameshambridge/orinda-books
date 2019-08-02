22cc16743c066614f9caaf446c7fd8aadcd86d61
Sam Hambridge <samhambridge@hotmail.com>
2019-08-02 12:45:33 +0100

    Updates

---
+++ precache-manifest.70896b7d0b160a0bb679274bd5609aeb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce8a01c8005fc0b1ea2440d0799e60e",
+    "url": "/orinda-books/index.html"
+  },
+  {
+    "revision": "89d3492e0e866b971fd9",
+    "url": "/orinda-books/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "82f7c37e160b0c478c24",
+    "url": "/orinda-books/static/css/main.e867f0fc.chunk.css"
+  },
+  {
+    "revision": "89d3492e0e866b971fd9",
+    "url": "/orinda-books/static/js/2.affc9ccc.chunk.js"
+  },
+  {
+    "revision": "82f7c37e160b0c478c24",
+    "url": "/orinda-books/static/js/main.2b929d28.chunk.js"
+  },
+  {
+    "revision": "d1c993c53202d04c0bf2",
+    "url": "/orinda-books/static/js/runtime~main.c17a579d.js"
+  },
+  {
+    "revision": "e6dbe74b84bc8155ef262ce8b5741592",
+    "url": "/orinda-books/static/media/Montserrat-Regular.e6dbe74b.ttf"
+  },
+  {
+    "revision": "9eec37f0efbc0f8044645476e284e741",
+    "url": "/orinda-books/static/media/inventory.9eec37f0.png"
+  },
+  {
+    "revision": "8bfeacd2fee6af7c3d4b49686857d814",
+    "url": "/orinda-books/static/media/mvboli.8bfeacd2.ttf"
+  },
+  {
+    "revision": "4304f43275086daecc81676128b651f5",
+    "url": "/orinda-books/static/media/orders.4304f432.png"
+  },
+  {
+    "revision": "8a02731f0874957d3052e52f7ba36f14",
+    "url": "/orinda-books/static/media/orinda-books-logo.8a02731f.png"
+  },
+  {
+    "revision": "0bc27cf2780a9b72093f2073df031c76",
+    "url": "/orinda-books/static/media/sales.0bc27cf2.png"
+  },
+  {
+    "revision": "8817ca0eb43556ee45d78d2d2c309926",
+    "url": "/orinda-books/static/media/search.8817ca0e.png"
+  },
+  {
+    "revision": "49153feb60983e9f6e6c6ccd9ed52e86",
+    "url": "/orinda-books/static/media/staff.49153feb.png"
+  }
+]);
